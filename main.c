58d3982a504bfd68a63f1ee00fcfcd1142c1b92e
xayrullaevziyovuddin <120357742+xayrullaevziyovuddin@users.noreply.github.com>
2024-03-15 12:51:25 +0000

    endsort

--- main.c
+++ main.c
@@ -4,33 +4,121 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-    int fd = open("./us_people_names.txt", O_RDONLY);
+typedef struct Students
+{
+    char *name;
+} students;
 
-    // Создаем буфер и выделяем память на начальные 1024 байта
-    char *buff = calloc(1024, sizeof(char));
+char *read_line(int fd)
+{
+    char *full_line = calloc(2, sizeof(char));
+    int bytes_read;
+    char buff = 0;
+    for (int i = 0;; i++)
+    {
+        bytes_read = read(fd, &buff, 1);
+  
+        if (bytes_read == 0 || buff == '\n')
+        {
+            full_line[i] = '\0';
+            break;
+        }
+        full_line[i] = buff;
+        full_line = realloc(full_line, (i + 2) * sizeof(char));
+    
+    }
+    return full_line;
+}
 
+students *load_to_struct(char *full_line)
+{
+    students *students1 = malloc(sizeof(students));
+    students1->name = strdup(full_line);
+    return students1;
+}
 
-    size_t total_size = 1024; // Общий размер буфера
-    size_t read_size = 0;     // Количество байт, прочитанных из файла
-    ssize_t bytes_read;       // Количество байт, прочитанных на этом шаге
+students **load_txt_data(char *file_name, int *size)
+{
+    int fd = open(file_name, O_RDONLY);
+    int count = 0;
+    char *full_line;
+    students **students_array = NULL; // Инициализация массива
 
-    // Читаем данные из файла
-    while ((bytes_read = read(fd, buff + read_size, 1024)) > 0) {
-        read_size += bytes_read;
-        total_size += 1024; // Увеличиваем общий размер буфера
-        buff = realloc(buff, total_size); // Увеличиваем размер буфера
-   
+    while (1)
+    {
+        full_line = read_line(fd);
+        if (full_line == NULL)
+        {
+            // Ошибка чтения строки
+            break;
+        }
+        if (strlen(full_line) == 0)
+        {
+            free(full_line);
+            break;
+        }
+        students *students1 = load_to_struct(full_line);
+        students_array = realloc(students_array, (count + 1) * sizeof(students *));
+        if (students_array == NULL)
+        {
+            // Обработка ошибки realloc
+            free(students1->name);
+            free(students1);
+            free(full_line);
+            break;
+        }
+        students_array[count++] = students1;
+        free(full_line);
     }
 
-
-
-    // Данные успешно прочитаны, выводим их
-    printf("%s\n", buff);
-
-    // Освобождаем память и закрываем файл
-    free(buff);
     close(fd);
+    *size = count;
+    return students_array;
+}
+
+void swap(students **arr, int i, int j)
+{
+    students *temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+void bubble_sort(students **arr, int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = 0; j < size - i - 1; j++)
+        {
+            if (strcmp(arr[j]->name, arr[j + 1]->name) > 0)
+            {
+                swap(arr, j, j + 1);
+            }
+        }
+    }
+}
+
+int main()
+{
+    char *file_name = "us_people_names.txt";
+    int array_size;
+    students **students_array = load_txt_data(file_name, &array_size);
+
+    if (students_array != NULL)
+    {
+        bubble_sort(students_array, array_size);
+
+        for (int i = 0; i < array_size; i++)
+        {
+            printf("Name: %s\n", students_array[i]->name);
+            free(students_array[i]->name);
+            free(students_array[i]);
+        }
+        free(students_array);
+    }
+    else
+    {
+        printf("Failed to load data from file.\n");
+    }
 
     return 0;
 }
